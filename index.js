ea367a4acd6d45264c9c36e5bdb4aac07c29a991
Shayan Malik <shayaaanmalik@gmail.com>
2026-08-16 21:25:05 +0500

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 let products = [];
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
